1f3a497c1c4e2351bf0283353fdc9a2e1382073a
Samilly <samillynunes19@gmail.com>
2024-05-13 15:28:15 -0300

    navigating within functions

--- src/pages/routes/index.jsx
+++ src/pages/routes/index.jsx
@@ -1,6 +1,23 @@
 import Link from "next/link";
+import router, { useRouter } from "next/router";
 
 export default function routes(){
+
+    function simpleNavigation(){
+        router.push('/routes/params?id=12&name=Samilly&teste=Nunes')
+    }
+
+    function navigateWithParams(){
+        router.push({
+            pathname: "routes/params",
+            query: {
+                id: 456,
+                name: "Leo",
+                teste: "Santos"
+            }
+        });
+    }
+
     return (
         <div>
             <h1>Rotas index</h1>
@@ -14,6 +31,19 @@ export default function routes(){
                     <li>Search 123</li>
                 </Link>
             </ul>
+            <br/><hr/><br/>
+
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start"
+            }} >
+                <button onClick={()=> router.push('/routes/123/search')} >Buscar</button>
+                <br/>
+                <button onClick={()=>simpleNavigation()} >Buscar params</button>
+                <button onClick={navigateWithParams} >Buscar com params</button>
+
+            </div>
         </div>
     );
 }
