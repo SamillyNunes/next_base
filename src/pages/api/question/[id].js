95c2f309479c4eb94c5643be1c29a6799d6d0c89
Samilly <samillynunes19@gmail.com>
2024-05-13 16:56:13 -0300

    api: separating from general method

--- src/pages/api/question/[id].js
+++ src/pages/api/question/[id].js
@@ -1,16 +1,20 @@
 export default function question(req, res){
     if(req.method==="GET"){
-        const id = req.query.id;
-
-        res.status(200).json({
-            id, //quando coloca so o valor, fica chave e valor "igual"
-            question: "Qual é a sua cor preferida?",
-            answers: [
-                'Branca', 'Amarela', 'Rosa', 'Verde'
-            ],
-        });
+        get(req, res);
     } else {
         // 405 eh method not allowed
         req.status(405).send();
     }
+}
+
+function get(req, res){
+    const id = req.query.id;
+
+    res.status(200).json({
+        id, //quando coloca so o valor, fica chave e valor "igual"
+        question: "Qual é a sua cor preferida?",
+        answers: [
+            'Branca', 'Amarela', 'Rosa', 'Verde'
+        ],
+    });
 }
