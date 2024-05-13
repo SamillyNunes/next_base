e41d99d6005c5563ba195cf86310c08f8f1d9c50
Samilly <samillynunes19@gmail.com>
2024-05-13 14:52:14 -0300

    routes: getting params

---
+++ src/pages/routes/[id]/params.jsx
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+
+export default function params(){
+    const router = useRouter();
+    const name = router.query.name;
+    const teste = router.query.teste;
+    console.log(router);
+
+    return (
+        <div>
+            <h1> Rotas params: {name} e {teste} </h1>
+        </div>
+    );
+}
